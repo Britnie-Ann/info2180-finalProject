a4c117a3488a0f400d13b4bc7f7bd0b4a7b4d544
Debra-Kaye Smith <debrakayesam@gmail.com>
2024-12-15 23:42:28 -0500

    modified JS and PHP, added a HTML and CSS file

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
   const addNoteForm = document.querySelector("#addNoteForm");
   const notesContainer = document.querySelector("#notesContainer");
+  const addUserForm = document.querySelector("#addUserForm");
+  const usersTextContainer = document.querySelector("#usersTextContainer");
 
-  addNoteForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  if (addNoteForm){
+    addNoteForm.addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    const formData = new FormData(addNoteForm);
+      const formData = new FormData(addNoteForm);
 
-    fetch("add_notes.php", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          notesContainer.innerHTML = data.html;
-          addNoteForm.reset();
-        } else if (data.error) {
-          alert("Error: " + data.error);
-        }
+      fetch("add_notes.php", {
+        method: "POST",
+        body: formData,
       })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("An unexpected error occurred.");
-      });
-  });
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            notesContainer.innerHTML = data.html;
+            addNoteForm.reset();
+          } else if (data.error) {
+            alert("Error: " + data.error);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("An unexpected error occurred.");
+        });
+    });
+  }
+
+  if (addUserForm){
+    addUserForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const formData = new FormData(addUserForm);
+
+      fetch("../php/add_users.php", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          usersTextContainer.innerHTML = data.html;
+          usersTextContainer.style.display = "block";
+          loadUserPage(data);
+        })  
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("An unexpected error occurred.");
+        });
+    });
+  }
 });
+
+function loadUserPage(response) {
+  setTimeout(function () {
+    if (response === "User Added Successfully!") {
+      window.location.assign("../php/dashboard.php");
+    } else {
+      window.location.assign("../pages/newUsers.html");
+    }
+  }, 2000);
+}
